1238ee0aa2b3a1746e9a929d503d302e620467a4
Tschipcraft <78470530+Tschipcraft@users.noreply.github.com>
2021-07-25 19:29:21 +0200

    Create get-mc-version.sh

---
+++ .github/workflows/get-mc-version.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Derives the MC Version from the package version.
+pack_version=$(jq '.pack.pack_format' pack.mcmeta)
+case "$pack_version" in
+  1 )
+    echo ::set-output name=MC_VERSION::"1.6.1-1.8.9"
+    ;;
+  2 )
+    echo ::set-output name=MC_VERSION::"1.9-1.10.2"
+    ;;
+  3 )
+    echo ::set-output name=MC_VERSION::"1.11-1.12.2"
+    ;;
+  4 )
+    echo ::set-output name=MC_VERSION::"1.13-1.14.4"
+    ;;
+  5 )
+    echo ::set-output name=MC_VERSION::"1.15-1.16.1"
+    ;;
+  6 )
+    echo ::set-output name=MC_VERSION::"1.16.2-1.16.3"
+    ;;
+  7 )
+    echo ::set-output name=MC_VERSION::"1.17"
+    ;;
+esac
